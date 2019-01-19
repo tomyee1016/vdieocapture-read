8afb53aed103b0d528139a75b42ca604830854a2
TomYee <40827861+tomyee1016@users.noreply.github.com>
2019-01-19 14:40:32 +0800

    Add files via upload

---
+++ imread.py
@@ -0,0 +1,16 @@
+import cv2
+cap =cv2.VideoCapture(0)
+while(True):
+    #Capture frame-by-frame一帧一帧的捕获
+    ret,frame = cap.read()
+    # Our operations on the frame come here
+    #gray =cv2.cvtColor(frame,cv2.COLOR_BGR2GRAY)
+
+    #Display the resulting frame
+    #cv2.imshow('frame',gray)
+    cv2.imshow('frame',frame)
+    if cv2.waitKey(1)& 0xff == ord('q'):
+        break
+#when everything done,release the capture
+cap.release()
+cv2.destroyAllWindows()
